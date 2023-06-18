6e3b5ffad816b7633ee7b241a8d77f391c7d98f4
Diego G <d.g.g.87@hotmail.com>
2023-06-18 03:21:55 -0600

    "agregare depositos"

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let sumMaxWithdraw = 0;
 let multWithdraw = 50;
 let donation = 0;
 let inputValue;
+let historial = [];
 
 const textScreen = document.getElementById('text-screen');
 
@@ -37,19 +38,51 @@ const divTextR0 = document.getElementById('text-r-0');
 
 // ----------------------------------------------------------------------------
 
+btnL0.addEventListener('click', inicio);
 btnL1.addEventListener('click', numTable);
-btnL0.addEventListener('click', mainMenu);
+btnL3.addEventListener('click', saldo);
+btnL4.addEventListener('click', quejas);
+btnL5.addEventListener('click', movimientos);
 
 // ----------------------------------------------------------------------------
 
 inicio();
 
 function inicio() {
+  limpiarElementos(divTextL1, divTextL2, divTextL3, divTextL4, divTextL5);
+  btnL1.removeEventListener('click', numTable);
   textScreen.innerHTML = 'B i e n v e n i d o';
   setTimeout(() => {
     mainMenu();
+    btnL1.addEventListener('click', numTable);
+    btnL3.addEventListener('click', saldo);
+    btnL4.addEventListener('click', quejas);
   }, 3000);
 }
+
+// ------------------------------------------------------------------------------
+
+function limpiarElementos(...elementos) {
+  elementos.forEach((elemento) => {
+    if (elemento) {
+      elemento.innerText = '';
+    }
+  });
+}
+
+// ----------------------------------------------------------------------------------------
+
+function mainMenu() {
+  divTextL1.innerText = `Retirar (multiplos de $50)`;
+  divTextL2.innerText = `Depositos (multiplos de $50)`;
+  divTextL3.innerText = `Consultar Saldo`;
+  divTextL4.innerText = `Quejas`;
+  divTextL5.innerText = `Ultimo Movimiento`;
+  divTextR0.innerText = 'Exit';
+  textScreen.innerText = '';
+}
+
+// ------------------------------------------------------------------------------------
 
 function retirarDinero(numero) {
   if (
@@ -61,6 +94,7 @@ function retirarDinero(numero) {
     sumMaxWithdraw += numero;
     textScreen.innerText = `Current balance: $${balance}
       Available: ${maxWithdraw - sumMaxWithdraw}`;
+    agregarHistorial(numero, balance);
     setTimeout(() => {
       donacion();
     }, 2500);
@@ -84,22 +118,139 @@ function retirarDinero(numero) {
   }
 }
 
-function mainMenu() {
-  divTextL1.innerText = `Retirar (multiplos de $50)`;
-  divTextL2.innerText = `Depositos (multiplos de $50)`;
-  divTextL3.innerText = `Consultar Saldo`;
-  divTextL4.innerText = `Quejas`;
-  divTextL5.innerText = `Ultimo Movimiento`;
-  divTextR0.innerText = 'Exit';
-  textScreen.innerText = '';
+// --------------------------------------------------------------------------------
+
+function saldo() {
+  textScreen.innerText = `Saldo Actual:  $${balance}`;
+  limpiarElementos(divTextL1, divTextL2, divTextL3, divTextL4, divTextL5);
+  setTimeout(() => {
+    mainMenu();
+  }, 2500);
 }
 
+// --------------------------------------------------------------------------------
+
+function quejas() {
+  textScreen.innerText =
+    'No disponible por el momento, favor de comunicarse al 000-000-000';
+  limpiarElementos(divTextL1, divTextL2, divTextL3, divTextL4, divTextL5);
+  setTimeout(() => {
+    mainMenu();
+  }, 2500);
+}
+
+// ------------------------------------------------------------------------------------
+
+function movimientos() {
+  limpiarElementos(divTextL1, divTextL2, divTextL3, divTextL4, divTextL5);
+  textScreen.innerHTML = `<p>Ultima operacion:</p>
+    <div>
+          <p>Cantidad retirada: ${
+            historial[historial.length - 1]['Cantidad retirada']
+          }</p>
+              <p>Cantidad restante: ${
+                historial[historial.length - 1]['Cantidad restante']
+              }</p>
+          <p>Fecha: ${historial[historial.length - 1].fecha}</p>
+    </div>
+  `;
+}
+
+// ------------------------------------------------------------------------------------
+
 function numTable() {
-  divTextL1.innerText = '';
-  divTextL2.innerText = '';
-  divTextL3.innerText = '';
-  divTextL4.innerText = '';
-  divTextL5.innerText = '';
+  btnL3.removeEventListener('click', saldo);
+  btnL4.removeEventListener('click', quejas);
+  creacionNumerosHTML();
+
+  const cantidadParagraph = document.getElementById('cantidad');
+  const submitValue = document.getElementById('btn-enter-id');
+
+  const btnNums = [...document.getElementsByClassName('btn-num')];
+  btnNums.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      ingresarNumero(btn.innerText);
+    });
+  });
+
+  // ------
+
+  function ingresarNumero(numero) {
+    const currentNumber = cantidadParagraph.innerText;
+    if (numero === 'clear') {
+      cantidadParagraph.innerText = '0';
+    } else {
+      cantidadParagraph.innerText =
+        currentNumber === '0' ? numero : currentNumber + numero;
+    }
+  }
+
+  submitValue.addEventListener('click', function () {
+    const cantidadValue = parseInt(cantidadParagraph.innerText);
+    retirarDinero(cantidadValue);
+  });
+}
+
+// ------------------------------------------------------------------------------------
+
+function donacion() {
+  textScreen.innerHTML = `Desea donar $200 pesos?
+
+  <div class="div-btn-yes-no">
+  <button class="btn-yes" id="btn-enter-si" type="submit">Si</button>
+  <button class="btn-no" id="btn-enter-no" type="submit">No</button>
+  </div>`;
+
+  const siButton = document.getElementById('btn-enter-si');
+  const noButton = document.getElementById('btn-enter-no');
+
+  siButton.addEventListener('click', () => {
+    if (balance >= 200) {
+      balance -= 200;
+      donation += 200;
+      textScreen.innerText = `Gracias! 
+  
+      Usted dono $200 pesos
+  
+      Fondos: $${balance}`;
+      setTimeout(() => {
+        mainMenu();
+      }, 3000);
+    } else {
+      textScreen.innerText = 'No cuenta con fondos suficientes';
+    }
+  });
+  noButton.addEventListener('click', () => {
+    textScreen.innerText = `Bonito dia!`;
+    setTimeout(() => {
+      mainMenu();
+    }, 2500);
+  });
+}
+
+// -------------------------------------------------------------------------
+
+function agregarHistorial(retiro, restante) {
+  const fecha = new Date();
+
+  const operacion = {
+    'Cantidad retirada': retiro,
+    'Cantidad restante': restante,
+    fecha:
+      fecha.getFullYear() +
+      '-' +
+      (fecha.getMonth() + 1) +
+      '-' +
+      fecha.getDate(),
+  };
+
+  historial.push(operacion);
+}
+
+// -------------------------------------------------------------------------
+
+function creacionNumerosHTML() {
+  limpiarElementos(divTextL1, divTextL2, divTextL3, divTextL4, divTextL5);
 
   textScreen.innerHTML = `
     <p>Balance: $${balance}</p>
@@ -127,65 +278,4 @@ function numTable() {
       <button class="btn-num btn-clear" id="btn-clear">clear</button>
     </div>
   `;
-
-  const cantidadParagraph = document.getElementById('cantidad');
-  const submitValue = document.getElementById('btn-enter-id');
-  const btnClear = document.getElementById('btn-clear');
-
-  const btnNums = [...document.getElementsByClassName('btn-num')];
-  btnNums.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      ingresarNumero(btn.innerText);
-    });
-  });
-
-  function ingresarNumero(numero) {
-    const currentNumber = cantidadParagraph.innerText;
-    if (numero === 'clear') {
-      cantidadParagraph.innerText = '0';
-    } else {
-      cantidadParagraph.innerText =
-        currentNumber === '0' ? numero : currentNumber + numero;
-    }
-  }
-
-  submitValue.addEventListener('click', function () {
-    const cantidadValue = parseInt(cantidadParagraph.innerText);
-    retirarDinero(cantidadValue);
-  });
-}
-
-function donacion() {
-  textScreen.innerHTML = `Desea donar $200 pesos?
-
-  <div class="div-btn-yes-no">
-  <button class="btn-yes" id="btn-enter-si" type="submit">Si</button>
-  <button class="btn-no" id="btn-enter-no" type="submit">No</button>
-  </div>`;
-
-  const siButton = document.getElementById('btn-enter-si');
-  const noButton = document.getElementById('btn-enter-no');
-
-  siButton.addEventListener('click', () => {
-    if (balance >= 200) {
-      balance -= 200;
-      donation += 200;
-      textScreen.innerText = `Gracias! 
-  
-      Usted dono $${donation} pesos
-  
-      Fondos: $${balance}`;
-      setTimeout(() => {
-        mainMenu();
-      }, 3000);
-    } else {
-      textScreen.innerText = 'No cuenta con fondos suficientes';
-    }
-  });
-  noButton.addEventListener('click', () => {
-    textScreen.innerText = `Bonito dia!`;
-    setTimeout(() => {
-      mainMenu();
-    }, 2500);
-  });
 }
